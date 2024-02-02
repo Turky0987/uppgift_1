01e707374c3bdd0255f268712fcccb791b2a6391
Turky0987 <144216105+Turky0987@users.noreply.github.com>
2024-02-02 12:24:03 +0100

    Add project files.

---
+++ uppgift 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> namnLista = new List<string>();
+
+        while (true)
+        {
+            Console.Write("Skriv in ett namn (tryck enter för att avsluta): ");
+            string namn = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(namn))
+                break; 
+
+            namnLista.Add(namn);
+        }
+
+        Console.WriteLine("\nNamnen i omvänd bokstavsordning:");
+        for (int i = namnLista.Count - 1; i >= 0; i--)
+        {
+            string omvandNamn = new string(namnLista[i].ToCharArray().Reverse().ToArray());
+            Console.WriteLine(omvandNamn);
+        }
+    }
+}
